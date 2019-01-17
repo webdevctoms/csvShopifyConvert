7033810c0f4f36f81b43685b14fb9476326b0795
Michael Peterson <WebDev@ctoms.ca>
2019-01-17 14:32:40 -0700

    added filter by item code functionality

--- index.js
+++ index.js
@@ -2,6 +2,39 @@ let netsuiteToShopify = {
 	//Name:
 };
 
+function filterByItemCodes(splitArr){
+	let newArr = [];
+	let splitItem;
+	if(Object.keys(itemCodes).length === 0){
+		return splitArr;
+	}
+	for(let i = 0;i < splitArr.length; i++){
+		if(i === 0){
+			newArr.push(splitArr[i])
+		}
+
+		if(itemCodes[splitArr[i][0] + "\r"]){
+			newArr.push(splitArr[i]);
+			continue;
+		}
+		splitItem = splitArr[i][0].split("-");
+		if(splitItem[0].length <= 1){
+			if(itemCodes[splitItem[1] + "\r"]){
+				newArr.push(splitArr[i]);
+				continue;
+			}
+		}
+		else if(splitItem[0].length > 1){
+			if(itemCodes[splitItem[0] + "\r"]){
+				newArr.push(splitArr[i]);
+				continue;
+			}
+		}
+	}
+
+	return newArr;
+}
+
 function formatString(fileString){	
 	let fileArray = fileString.split(",");
 	//console.log("in function: ", fileString);
@@ -95,29 +128,49 @@ function splitArray(newlineArray){
 	return returnArray;
 }
 
-function readFile(files) {
+function readFile(files, type) {
 
     var file = files[0];           
     var reader = new FileReader();
     reader.onload = function(event) {
+    	if(type === "filter"){
+    		let fileString = event.target.result; 
+	    	let fileArrayByNewLines = fileString.split("\n");
+	    	//just need to remember to put new line at the end of the array
+	    	fileArrayByNewLines.pop();
+	    	//console.log(fileArrayByNewLines); 
+	 
+	    	let commaSplitArray = splitArray(fileArrayByNewLines);
+	    	addBlank(commaSplitArray);
+	    	verifyLength(commaSplitArray);
 
-    	let fileString = event.target.result; 
-    	let fileArrayByNewLines = fileString.split("\n");
-    	//just need to remember to put new line at the end of the array
-    	fileArrayByNewLines.pop();
-    	console.log(fileArrayByNewLines); 
- 
-    	let commaSplitArray = splitArray(fileArrayByNewLines);
-    	addBlank(commaSplitArray);
-    	verifyLength(commaSplitArray);
+	    	let removedSplitArray = removeForPurchase(commaSplitArray);
+	    	verifyForPurchaseRemoved(removedSplitArray);
+	    	console.log(removedSplitArray);
 
-    	let removedSplitArray = removeForPurchase(commaSplitArray);
-    	verifyForPurchaseRemoved(removedSplitArray);
-    	console.log(removedSplitArray);
+	    	let filteredByItemCodesArray = filterByItemCodes(removedSplitArray); 
+	    	console.log(filteredByItemCodesArray);
+    	}
+    	else if(type === "submit"){
+    		let fileString = event.target.result; 
+	    	let fileArrayByNewLines = fileString.split("\n");
+	    	//just need to remember to put new line at the end of the array
+	    	fileArrayByNewLines.pop();
+	    	console.log(fileArrayByNewLines); 
+	 
+	    	let commaSplitArray = splitArray(fileArrayByNewLines);
+	    	addBlank(commaSplitArray);
+	    	verifyLength(commaSplitArray);
 
-    	formatString(fileString);     
+	    	let removedSplitArray = removeForPurchase(commaSplitArray);
+	    	verifyForPurchaseRemoved(removedSplitArray);
+	    	console.log(removedSplitArray);
+
+	    	formatString(fileString); 
+    	}    
     }
     reader.readAsText(file)
+    	
 }
 
 function submitClicked(){
@@ -126,12 +179,23 @@ function submitClicked(){
 		console.log("submit clicked");
 		let csvFile = $('#inputFile').prop('files');
 		//console.log(csvFile);
-		readFile(csvFile);
+		readFile(csvFile,"submit");
+	})
+}
+
+function filterClicked(){
+	$("#jsFilter").click(function(event){
+		event.preventDefault();
+		//console.log("filter clicked", JSON.stringify(itemCodes));
+		let csvFile = $('#inputFile').prop('files');
+		//console.log(csvFile);
+		readFile(csvFile,"filter");
 	})
 }
 
 function initPage(){
 	submitClicked();
+	filterClicked();
 }
 
 $(initPage);
